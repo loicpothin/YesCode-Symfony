99fc8e371627ba399f89665bf0595a7ce2efcfd0
loicpothin69 <loicpothin69@gmail.com>
2021-01-08 09:45:58 +0100

    Injection de Dépendance
    Injection du manager dans notre public function index
    on crée deux objets on les persist en application pour un eventuel futur envoi
    Puis on flush -> cela attéri bien en bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Fruit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,12 +13,34 @@ class HomeController extends AbstractController{
      * @Route("/", name="home_page")
      */
 
-     public function index(): Response
+     public function index(EntityManagerInterface $manager): Response
       {
-         return $this->render('home/index.html.twig',[
-             
-             
-         ]);
+
+        $banane= new Fruit();
+
+        $fraise= new Fruit();
+
+        $tomate= new Fruit();
+
+        $banane->setName("banane plantain");
+        $fraise->setName("charlotte au fraise");
+        $tomate->setName("tomate verte");
+
+        // dit a doctrine qu'on veut sauvegarder le produit 
+        $manager->persist($banane);
+
+        $manager->persist($fraise);
+
+        $manager->persist($tomate);
+        // Flush pour tout ! Excecute la requete 
+        $manager->flush();
+        
+        // Le tout se déclenche en actualisant la page 
+        // Provisoire bien sur 
+
+       return $this->render('home/index.html.twig',[
+        
+       ]);
      }
 
      
