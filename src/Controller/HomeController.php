c34a8be57b8e05a044677870b98228d8062d14e7
loicpothin69 <loicpothin69@gmail.com>
2021-01-08 10:18:46 +0100

    Repository Concept
    Récupération des données en BDD via le repo dans le controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,45 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Fruit;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\FruitRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController{
     /**
-     * @Route("/", name="home_page")
-     */
+    * @Route("/", name="home_page")
+    */
 
-     public function index(EntityManagerInterface $manager): Response
-      {
-
-        $banane= new Fruit();
-
-        $fraise= new Fruit();
-
-        $tomate= new Fruit();
-
-        $banane->setName("banane plantain");
-        $fraise->setName("charlotte au fraise");
-        $tomate->setName("tomate verte");
-
-        // dit a doctrine qu'on veut sauvegarder le produit 
-        $manager->persist($banane);
-
-        $manager->persist($fraise);
-
-        $manager->persist($tomate);
-        // Flush pour tout ! Excecute la requete 
-        $manager->flush();
+    public function index(FruitRepository $repo): Response
+    {
+        $fruits = $repo->findAll();
+        // 1er methode 
+        //Permet d"afficher tous ce qu'il y dans la BBD dans le profiler (bas de page du site )
+        dump($fruits);
         
-        // Le tout se déclenche en actualisant la page 
-        // Provisoire bien sur 
+      return $this->render('home/index.html.twig',[
+        // 2er methode 
+        // permet d'afficher tous ce qu'il y dans la BBD coté vue (page site)
+        //  {{ dump(fruits) }} A ajouter coter vue donc dans index
+        "fruits" => $fruits
 
-       return $this->render('home/index.html.twig',[
-        
-       ]);
-     }
+      ]);
+    }
 
      
 }
