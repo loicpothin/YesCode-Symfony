7cc2c3cab9227e763caee40f792acc12daf3f1a1
loicpothin69 <loicpothin69@gmail.com>
2021-01-12 12:23:02 +0100

    fin du test Faker - Librairie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use Faker;
+
 use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-
   /**
    * @Route("/", name="home_page")
    */
@@ -19,29 +18,9 @@ class HomeController extends AbstractController
   {
     $articles = $repo->findLastArticles(3);
 
-    // librairie Faker (telechager ds le terminal en --dev )
-    $faker = Faker\Factory::create('fr_FR');
-
-    // $intro = $faker->sentence(2);
-
-    // $intro = $faker->paragraphs(2);
-
-    // $contenu = ["pomme", "poire", "figue", "grenade", "peche"];
-
-    $content = "<p>" . implode("</p><p>" , $faker->paragraphs(8)) . "<p>";
-
-    $createdAt = $faker->dateTimeBetween(' - 3 months');
-  
-    dump( $createdAt);
-
-    // lorem Picsum génere des photo en boucle 
-    $image = "https://picsum.photos/400/300";
-
-
     return $this->render('home/index.html.twig', [
       "articles" => $articles,
-      "image" => $image,
-      "content" => $content
+      
     ]);
   }
 }
