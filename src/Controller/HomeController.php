80a101176c528b3d315d2617fddb1daa131d8630
loicpothin69 <loicpothin69@gmail.com>
2021-01-12 14:26:19 +0100

    import et test de Slugify -  sur le Home
    tranforme une props d'une entité en slug valide pour naviguer via le slug (URL)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use App\Repository\ArticleRepository;
+use Cocur\Slugify\Slugify;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +18,14 @@ class HomeController extends AbstractController
   public function index(ArticleRepository $repo): Response
   {
     $articles = $repo->findLastArticles(3);
+
+
+    $slugify = new Slugify();
+
+    $title ="la théorie des cordes à linges Gravitationnelles";
+
+    $slug = $slugify->slugify($title);
+    dump($slug);
 
     return $this->render('home/index.html.twig', [
       "articles" => $articles,
