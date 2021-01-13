73d0c9d6a5188f34b070f89902c603664e78708c
loicpothin69 <loicpothin69@gmail.com>
2021-01-13 14:40:57 +0100

    Form-Customisation-Label Placeholder
    modification du visuel dans ArticleType
    doc:
    Form Types Reference

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,23 +4,38 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('intro')
-            ->add('content')
-            ->add('image')
+            ->add('title', TextType::class,[
+                'label' => 'Titre de l\'article',
+                'attr'  => ['placeholder' => 'Tapez votre titre ici !' ]
+            ])
+            ->add('intro', TextType::class,[
+                'label' => 'Titre de l\'article',
+                'attr'  => ['placeholder' => 'Une phrase d\'accroche !']
+            ])
+            ->add('content',TextareaType::class,[
+                'label' => 'Votre contenu',
+                'attr'  => ['placeholder' => 'Dites-nous tout !']
+            ])
+            ->add('image',UrlType::class,[
+                'label' => 'Adresse de l\'image',
+                'attr'  => ['placeholder' => 'Coller un lien d\'image !']
+            ])
             ->add('Envoyer', SubmitType::class)
         
 
-           
+             
         ;
     }
 
